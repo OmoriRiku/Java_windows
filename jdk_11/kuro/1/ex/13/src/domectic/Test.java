65129befed31620028c3dee2bdade36fbc0be1dc
OmoriRiku <oomori3321@gmail.com>
2023-08-26 01:15:25 +0900

    java silver資格取得

--- jdk_11/kuro/1/ex/13/src/domectic/Test.java
+++ jdk_11/kuro/1/ex/13/src/domectic/Test.java
@@ -1,7 +1,10 @@
+package domectic;
+import global.*;
+
 public class Test {
 	public static void main(String[] args) {
 		Customer cust = new WebCustomer();
-		cust.id = 152;
+		//cust.id = 152;
 		cust.setName("Rocio");
 	}
 }
--- jdk_11/kuro/1/ex/13/src/global/WebCustomer.java
+++ jdk_11/kuro/1/ex/13/src/global/WebCustomer.java
@@ -1,6 +1,6 @@
 package global;
 public class WebCustomer extends Customer {
-	private void printMessage() {
+	public void printMessage() {
 		System.out.println("Hola " + id + " " + accNo);
 	}
 }
