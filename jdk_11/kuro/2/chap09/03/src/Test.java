afc6fbfec234678387a51dff354eabf1b5aaefa7
OmoriRiku <oomori3321@gmail.com>
2023-09-10 23:39:20 +0900

    Java silverの資格勉強

--- jdk_11/kuro/2/chap09/03/src/Test.java
+++ jdk_11/kuro/2/chap09/03/src/Test.java
@@ -1,10 +1,18 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Comparator;
+
+class SampleComparator implements Comparator<Integer> {
+	@Override
+	public int compare(Integer a, Integer b) {
+		return a.compareTo(b);
+	}
+}
 
 class Test {
 	public static void main(String[] args) {
-		List<Integer> list = Arrays.asList(new Integer[] {1, 2, 3});
-		list.sort((a, b) -> -a.compareTo(b));
+		List<Integer> list = Arrays.asList(new Integer[] {3, 2, 1});
+		list.sort(new SampleComparator());
 		for (Integer num : list) {
 			System.out.println(num);
 		}
--- jdk_11/kuro/2/chap09/com/src/Test.java
+++ jdk_11/kuro/2/chap09/com/src/Test.java
@@ -1,4 +1,3 @@
-import java.util.Comparator;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,8 +6,8 @@ class Item {
 	private String name;
 
 	public Item(int id, String name) {
-		this.id = id;
 		this.name = name;
+		this.id = id;
 	}
 
 	public int getId() {
@@ -20,31 +19,23 @@ class Item {
 	}
 }
 
-class SampleComparator implements Comparator<Item> {
-	@Override
-	public int compare(Item s1, Item s2) {
-		if (s1.getId() < s2.getId()) {
-			return -1;
-		}
-		if (s2.getId() < s1.getId()) {
-			return 1;
-		}
-		return 0;
-	}
-}
-
 class Test {
 	public static void main(String[] args) {
 		List<Item> list = new ArrayList<>();
-		list.add(new Item(1, "A"));
-		list.add(new Item(3, "B"));
-		list.add(new Item(2, "C"));
+		list.add(new Item(3, "A"));
+		list.add(new Item(2, "B"));
+		list.add(new Item(1, "C"));
 
-		list.sort(new SampleComparator());
+		list.sort((Item i1, Item i2) -> {
+			if (i1.getId() < i2.getId()) {
+				return -1;
+			}
+			if (i1.getId() > i2.getId()) {
+				return 1;
+			}
+			return 0;
+		});
 
-		for (Item item : list) {
-			System.out.println(item.getId() + ":" + item.getName());
-		}
+		list.forEach(item -> System.out.println(item.getId() + ":" + item.getName()));
 	}
 }
-
